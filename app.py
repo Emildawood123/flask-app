24261269bc3f02f8e87d40361f93d38ee68ce1a4
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-04-14 16:24:12 +0000

    Okey

--- app.py
+++ app.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask import render_template
 
 app = Flask(__name__)
 
+JOPS = [
+    {
+        'id': 1,
+        'jop': 'backend engineer',
+        'company': 'google',
+        'location': 'bangalore',
+        'sallary': '5000$'
+    },
+    {
+        'id': 2,
+        'jop': 'frontend engineer',
+        'company': 'facebook',
+        'location': 'san fransico',
+        'sallary': '3000$'
+    },
+    {
+        'id': 3,
+        'jop': 'data analyst',
+        'company': 'amazon',
+        'location': 'Cario',
+        'sallary': '1500$'
+    },
+    {
+        'id': 4,
+        'jop': 'Doctor',
+        'company': 'microsoft',
+        'location': 'giza',
+        'sallary': '1300$'
+    },
+]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jops=JOPS)
+
+
+@app.route('/api/jops')
+def list_jops():
+  return jsonify(JOPS)
 
 
 if __name__ == "__main__":
