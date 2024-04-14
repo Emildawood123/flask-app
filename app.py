07d9ae3e56bf4237cde7049710ebcb1c55513d6b
Emildawood123 <66915790+Emildawood123@users.noreply.github.com>
2024-04-14 15:30:44 +0000

    style with css and bootstarp framework

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+  return render_template('home.html')
 
-@app.route("/login")
-def login():
-  return "<p>login, World!</p>"
-  
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
